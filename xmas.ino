26c5081b449aaecfb6cf9ba45f8277b14f41ba1e
project516 <project516.progess139@slmail.me>
2025-12-22 14:01:45 -0600

    document code, split code into lights and sound

--- xmas.ino
+++ xmas.ino
@@ -1,6 +1,7 @@
 #include "pitches.h"
 
-int switchState = 0;
+int lightSwitchState = 0;
+int soundSwitchState = 0;
 
 int melody[] = {
   NOTE_C4, NOTE_G3, NOTE_A3, NOTE_G3, 0 , NOTE_B3, NOTE_C4
@@ -11,12 +12,19 @@ int noteDurations[] = {
 };
 
 void setup() {
-  // put your setup code here, to run once:
+  // setup code, runs once
+
+  // light button
   pinMode(2, INPUT);
+
+  // lights
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
+
+  // sound button
+  pinMode(10, INPUT);
 
   for (int thisNote = 0; thisNote < 8; thisNote++) {
     int noteDuration = 1000 / noteDurations[thisNote];
@@ -32,9 +40,11 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  switchState = digitalRead(2);
-  if (switchState == LOW) {
+  // main code, runs repeatedly
+
+  // lights
+  lightSwitchState = digitalRead(2);
+  if (lightSwitchState == LOW) {
     digitalWrite(3, LOW);
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
@@ -45,6 +55,12 @@ void loop() {
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
     digitalWrite(6, HIGH);
+
+  }
+
+  // sound
+  lightSwitchState = digitalRead(10);
+  if (soundSwitchState == LOW) {
 
   }
 
