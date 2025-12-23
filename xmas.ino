4306db8419752d32c0e99237838045bbb461c523
project516 <project516.progress139@slmail.me>
2025-12-23 13:48:51 -0600

    refactor code

--- xmas.ino
+++ xmas.ino
@@ -57,24 +57,22 @@ void loop() {
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
     digitalWrite(6, HIGH);
-    Serial.println("light button on");
 
   }
 
   // sound
   soundSwitchState = digitalRead(10);
-  
   if (soundSwitchState == HIGH) {
-    Serial.println("sound button on");
+    playSound();
+  }
+}
 
-      for (int thisNote = 0; thisNote < 8; thisNote++) {
-
-        int noteDuration = 1000 / noteDurations[thisNote];
-        tone(8, melody[thisNote], noteDuration);
-        int pauseBetweenNotes = noteDuration * 1.30;
-        delay(pauseBetweenNotes);
-        noTone(8);
-
-    }
+void playSound() {
+  for (int thisNote = 0; thisNote < 7; thisNote++) {
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(8, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    noTone(8);
   }
 }
