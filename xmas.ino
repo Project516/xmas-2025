9bc206d7d2194575370ec18fa33dcf5460310837
project516 <project516.progress139@slmail.me>
2025-12-23 14:07:56 -0600

    jingle bells song, format code

--- xmas.ino
+++ xmas.ino
@@ -1,9 +1,7 @@
 /*
-
-  Light and sound code for 2025 Arduino XMAS project
-
-  Author: project516 <project516.progress139@slmail.me>
-  */
+ * Light and sound code for 2025 Arduino XMAS project
+ * Author: project516 <project516.progress139@slmail.me>
+ */
 
 #include "pitches.h"
 
@@ -11,30 +9,46 @@ int lightSwitchState = 0;
 int soundSwitchState = 0;
 
 int melody[] = {
-  NOTE_E5, NOTE_E5, NOTE_E5,  // Jingle bells
-  NOTE_E5, NOTE_E5, NOTE_E5,  // Jingle bells
-  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5, // Jingle all
-  NOTE_E5,                    // The way
-  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5, // Oh what fun
-  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, // It is to ride
+  NOTE_E5, NOTE_E5, NOTE_E5,  
+  NOTE_E5, NOTE_E5, NOTE_E5,  
+  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5, 
+  NOTE_E5,                    
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5, 
+  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5, 
   NOTE_E5, NOTE_D5, NOTE_D5, NOTE_E5,
-  NOTE_D5, NOTE_G5            // Open sleigh
+  NOTE_D5, NOTE_G5,
+
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_E5, NOTE_G5, NOTE_C5, NOTE_D5,
+  NOTE_E5,
+  NOTE_F5, NOTE_F5, NOTE_F5, NOTE_F5,
+  NOTE_F5, NOTE_E5, NOTE_E5, NOTE_E5, NOTE_E5,
+  NOTE_G5, NOTE_G5, NOTE_F5, NOTE_D5,
+  NOTE_C5
 };
 
-// Note durations: 4 = quarter note, 8 = eighth note, etc.
 int noteDurations[] = {
   8, 8, 4,
   8, 8, 4,
-  8, 8, 8, 8,
+  8, 8, 8, 8, 
   2,
   8, 8, 8, 8,
   8, 8, 8, 16, 16,
   8, 8, 8, 8,
-  4, 4
+  4, 4,
+
+  8, 8, 4,
+  8, 8, 4,
+  8, 8, 8, 8, 
+  2,
+  8, 8, 8, 8,
+  8, 8, 8, 16, 16,
+  8, 8, 8, 8,
+  1 
 };
 
 void setup() {
-  // setup code, runs once
 
   // light button
   pinMode(2, INPUT);
@@ -57,7 +71,6 @@ void setup() {
 }
 
 void loop() {
-  // main code, runs repeatedly
 
   // lights
   lightSwitchState = digitalRead(2);
@@ -66,13 +79,11 @@ void loop() {
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
     digitalWrite(6, LOW);
-
   } else {
     digitalWrite(3, HIGH);
     digitalWrite(4, HIGH);
     digitalWrite(5, HIGH);
     digitalWrite(6, HIGH);
-
   }
 
   // sound
@@ -83,7 +94,7 @@ void loop() {
 }
 
 void playSound() {
-  for (int thisNote = 0; thisNote < 26; thisNote++) {
+  for (int thisNote = 0; thisNote < 51; thisNote++) {
     int noteDuration = 1000 / noteDurations[thisNote];
     tone(8, melody[thisNote], noteDuration);
     int pauseBetweenNotes = noteDuration * 1.30;
